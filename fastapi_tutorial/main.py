642f25273e06255cd6319bafc84cbe74aff40a3b
Toshi-Yano <t.yano.0726.6119@gmail.com>
2024-02-17 07:52:43 +0900

    パスパラメータ
    https://fastapi.tiangolo.com/ja/tutorial/path-params/

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -1,8 +1,53 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
 
+# 基本
 @app.get("/")
 async def root():
     return {"message": "Hello World!!!"}
+
+
+# パラメータ
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+# 静的と動的（順序）
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+# enum
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+# file path
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
