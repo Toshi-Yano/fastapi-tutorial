aabe846ff74f87b83661dbdee287f4fb8933b89b
Toshi-Yano <t.yano.0726.6119@gmail.com>
2024-02-22 02:08:35 +0900

    パスパラメータと数値の検証
    https://fastapi.tiangolo.com/ja/tutorial/path-params-numeric-validations/

---
+++ fastapi_tutorial/4_main_parameter_numeric.py
@@ -0,0 +1,28 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Path, Query
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: Annotated[int, Path(title="The ID of the item to get")],
+    q: Annotated[str | None, Query(alias="item-query")] = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items/{item_id}")
+async def read_items2(
+    item_id: Annotated[int, Path(title="The ID of the item to get", gt=0, le=1000)],
+    q: str,
+    size: Annotated[float, Query(gt=0, lt=10.5)],
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
